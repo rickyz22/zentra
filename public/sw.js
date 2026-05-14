f963b0d3c4356836ba7773eccd59cbf2619ed64f
rickyz22 <rickyz1749@gmail.com>
2026-05-14 13:22:17 -0300

    feat(ui): Reemplazado prompt nativo por Modal Tailwind para la reprogramación de vencimientos

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zentra-v2.9.2'; // Incrementar esto junto con APP_VERSION
+const CACHE_NAME = 'zentra-v2.9.3'; // Incrementar esto junto con APP_VERSION
 const ASSETS = [
   '/',
   '/index.html',
