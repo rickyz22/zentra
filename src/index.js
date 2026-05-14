bfbd3dd3b32064bacb78f3d7ad1a1cc59089b827
rickyz22 <rickyz1749@gmail.com>
2026-05-14 16:13:17 -0300

    Fix: Sincronización global de versiones v3.3.4

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // VERSIÓN CANÓNICA DEL SERVIDOR
 // Actualizá este número con cada deploy
 // ==========================================
-const APP_VERSION = '3.3.0';
+const APP_VERSION = '3.3.4';
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
