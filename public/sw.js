748f3eda238bb2d2ebc73f88bf1069a226dc3a3e
rickyz22 <rickyz1749@gmail.com>
2026-05-14 13:50:04 -0300

    feat(ui): Rediseño del formulario de creación a Wizard paso a paso

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zentra-v2.9.6'; // Incrementar esto junto con APP_VERSION
+const CACHE_NAME = 'zentra-v2.9.8'; // Incrementar esto junto con APP_VERSION
 const ASSETS = [
   '/',
   '/index.html',
