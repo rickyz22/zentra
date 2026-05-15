f3dc3059a2f572610278cdc25ec3363c8e884e68
rickyz22 <rickyz1749@gmail.com>
2026-05-14 22:21:49 -0300

    chore: bump version to v3.5.2 to force cache refresh for comprobante modal

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zentra-v3.5.1-PRIVACY-FIX'; // v3.5.1-PRIVACY-UPGRADE
+const CACHE_NAME = 'zentra-v3.5.2-PRIVACY-FIX'; // v3.5.2-PRIVACY-UPGRADE
 const ASSETS = [
   '/',
   '/index.html',
